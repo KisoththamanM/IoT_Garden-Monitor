c7a9b5e3efe717e4c88ad786b62ac8c61b856ad6
KisoththamanM <mkisoththamanofficial@gmail.com>
2025-12-26 14:29:57 +0530

    Initial commit: Flutter and Arduino code

---
+++ Flutter App/Garden Monitor/garden_monitor/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: GardenMonitor(), debugShowCheckedModeBanner: false));
+}
+
+class GardenMonitor extends StatelessWidget {
+  const GardenMonitor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
